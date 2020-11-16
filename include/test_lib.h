7e6ee86cc72c29ee18908fe420d210de0784a574
Michał Wichrowski <mtwichrowski@gmail.com>
2020-11-16 14:18:32 +0100

    zajecia 16.11: programowanie obiektowe ciag dalszy

--- include/test_lib.h
+++ include/test_lib.h
@@ -18,27 +18,67 @@ double dodawanie_d(double a, double b);
 template <class T>
 T dodawanie(T a, T b);
 
-double dodaj(double a, double b) { return a + b; }
+inline double dodaj(double a, double b, int coeff = 1) { return a + b * coeff; }
 }  // namespace Tools
 
 template <class T>
 T odejmowanie(T a, T b);
 
+class B;
+
 class A {
  public:
   A();
-  A(int k);
+  explicit A(int k);
   A(int b, int c);
   A(const A &inne_A);
+  //  A(double k) = delete;
+
+  A(const B &b);
+  operator B() = delete;
+  // rzutowanie obiektu klasy A na B
+  //  A x
+  // (B)x;
+
+  const A &operator>(A &&drugie_a) {
+    if (&drugie_a != this) {
+      delete litera;
+      litera = new char(*drugie_a.litera);
+      //      delete drugie_a.litera;
+    }
+    return *this;
+  }
+
+  const A &operator=(const int &liczba) {
+    std::cout << "operator przypisania" << std::endl;
+    /*if (&drugie_a != this) {
+      delete litera;
+      litera = new char(*drugie_a.litera);
+    }*/
+    return *this;
+  }
+
+  A operator+(const A &drugie_a) const {
+    std::cout << "operator dodawania" << std::endl;
+    return A(this->numer + drugie_a.numer);
+  }
 
   ~A();
 
-  void b();
+  void b(A &drugie_a) const;
+  void sprawdzenie() const {
+    x = 50;
+    // blad!
+    // y = 8;
+  }
 
  private:
   const int numer;
   const int numer_2;
   char *litera;
+  // zmienna, która mogą modyfikować metody const
+  mutable int x;
+  int y;
 };
 
 #endif /* INCLUDE_TEST_LIB_H_ */
--- source/main.cc
+++ source/main.cc
@@ -9,39 +9,44 @@
 template <typename T>
 using mapa_1 = std::map<std::string, T>;
 
-
 using namespace Tools;
 
-int
-main()
-{
+int main() {
   A obiekt;
   A obiekt1(4);
   A obiekt2(1, 3);
 
-  A obiekt3 = 6;
+  A obiekt3 = A(6);
+  obiekt3 = 7;
 
   A obiekt4 = obiekt3;
 
   A(99, 99);
 
+  obiekt > obiekt4 + obiekt3;
+
   A obiekt5 = A(6, 8);
 
+  std::cout << "NIEBEZPIECZNA LINIA" << std::endl;
+  A obiekt6((int)6.5);
+  std::cout << "NIEBEZPIECZNA LINIA" << std::endl;
 
-  std::vector<double>           piatki(10, 5.);
+  //  obiekt1 = obiekt5 = obiekt3;
+  //  obiekt5 = obiekt5;
+
+  A(5);
+
+  std::vector<double> piatki(10, 5.);
   std::vector<std::vector<int>> tablica_2D(10, std::vector<int>(20, 6));
-
-
 
   return 0;
   std::cout << __FILE__ << std::endl;
 
   //  std::cout << PI_razy_dwa << std::endl;
 
-  printf("!!!Hello World!!!"); // prints !!!Hello World!!!
+  printf("!!!Hello World!!!");  // prints !!!Hello World!!!
 
   typedef std::map<std::string, int> mapa_2;
-
 
   double a = 4.;
   double b = 6.;
